42e0e578acd05e2141543d32400d9908fe087f20
kiduknott <5183125+kiduknott@users.noreply.github.com>
2025-02-07 19:36:50 +0000

    Added OpenAPI description for product-composite microservice

--- microservices/product-composite-service/src/main/java/com/akt/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/com/akt/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,8 +1,14 @@
 package com.akt.microservices.composite.product;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +18,37 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @ComponentScan("com.akt")
 public class ProductCompositeServiceApplication {
+
+	@Value("api.common.version") String apiVersion;
+	@Value("api.common.title") String apiTitle;
+	@Value("api.common.description") String apiDescription;
+	@Value("api.common.termsOfService") String apiTermsOfService;
+	@Value("${api.common.license}") String apiLicense;
+	@Value("${api.common.licenseUrl}") String apiLicenseUrl;
+	@Value("${api.common.externalDocDescription}") String apiExternalDocDescription;
+	@Value("api.common.externalDocUrl") String apiExternalDocUrl;
+	@Value("${api.common.contact.name}") String apiContactName;
+	@Value("${api.common.contact.url}") String apiContactUrl;
+	@Value("${api.common.contact.email}") String apiContactEmail;
+
+	@Bean
+	public OpenAPI getOpenApiDocumentation(){
+		return new OpenAPI()
+				.info(new Info().title(apiTitle)
+						.description(apiDescription)
+						.version(apiVersion)
+						.contact(new Contact()
+								.name(apiContactName)
+								.url(apiContactUrl)
+								.email(apiContactEmail))
+						.termsOfService(apiTermsOfService)
+						.license(new License()
+								.name(apiLicense)
+								.url(apiLicenseUrl)))
+				.externalDocs(new ExternalDocumentation()
+						.description(apiExternalDocDescription)
+						.url(apiExternalDocUrl));
+	}
 
 	@Bean
 	RestTemplate restTemplate() {
@@ -29,5 +66,4 @@ public class ProductCompositeServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
 	}
-
 }
--- util/src/main/java/com/akt/util/http/GlobalControllerExceptionHandler.java
+++ util/src/main/java/com/akt/util/http/GlobalControllerExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.akt.util.http;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
-
+import com.akt.api.exceptions.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -14,32 +12,42 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.akt.api.exceptions.InvalidInputException;
 import com.akt.api.exceptions.NotFoundException;
 
+import static org.springframework.http.HttpStatus.*;
+
 @RestControllerAdvice
 class GlobalControllerExceptionHandler {
 
   private static final Logger logger = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
 
+  @ResponseStatus(BAD_REQUEST)
+  @ExceptionHandler(BadRequestException.class)
+  public @ResponseBody HttpErrorInfo handleBadRequestExceptions(
+          ServerHttpRequest request, BadRequestException exception) {
+
+    return createHttpErrorInfo(BAD_REQUEST, request, exception);
+  }
+
   @ResponseStatus(NOT_FOUND)
   @ExceptionHandler(NotFoundException.class)
   public @ResponseBody HttpErrorInfo handleNotFoundExceptions(
-    ServerHttpRequest request, NotFoundException ex) {
+    ServerHttpRequest request, NotFoundException exception) {
 
-    return createHttpErrorInfo(NOT_FOUND, request, ex);
+    return createHttpErrorInfo(NOT_FOUND, request, exception);
   }
 
   @ResponseStatus(UNPROCESSABLE_ENTITY)
   @ExceptionHandler(InvalidInputException.class)
   public @ResponseBody HttpErrorInfo handleInvalidInputException(
-    ServerHttpRequest request, InvalidInputException ex) {
+    ServerHttpRequest request, InvalidInputException exception) {
 
-    return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, ex);
+    return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, exception);
   }
 
   private HttpErrorInfo createHttpErrorInfo(
-    HttpStatus httpStatus, ServerHttpRequest request, Exception ex) {
+    HttpStatus httpStatus, ServerHttpRequest request, Exception exception) {
 
     final String path = request.getPath().pathWithinApplication().value();
-    final String message = ex.getMessage();
+    final String message = exception.getMessage();
 
     logger.debug("Returning HTTP status: {} for path: {}, message: {}", httpStatus, path, message);
     return new HttpErrorInfo(httpStatus, path, message);
