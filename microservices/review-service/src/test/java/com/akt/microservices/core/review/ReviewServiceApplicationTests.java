8b419cb16729e18839ab0ec1ed2f348e7cdb8c05
kiduknott <5183125+kiduknott@users.noreply.github.com>
2025-02-10 16:22:56 +0000

    Test to get review by product Id

--- microservices/review-service/src/test/java/com/akt/microservices/core/review/ReviewServiceApplicationTests.java
+++ microservices/review-service/src/test/java/com/akt/microservices/core/review/ReviewServiceApplicationTests.java
@@ -1,21 +1,29 @@
 package com.akt.microservices.core.review;
 
+import com.akt.api.core.review.Review;
+import com.akt.microservices.core.review.persistence.ReviewEntity;
 import com.akt.microservices.core.review.persistence.ReviewRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static reactor.core.publisher.Mono.just;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class ReviewServiceApplicationTests extends MySqlTestBase {
 
 	@Autowired
-	private WebTestClient webTestClient;
+	private WebTestClient client;
 
 	@Autowired
 	private ReviewRepository repository;
@@ -24,11 +32,35 @@ class ReviewServiceApplicationTests extends MySqlTestBase {
 	void contextLoads() {
 	}
 
+	@BeforeEach
+	void setupDb() {
+		repository.deleteAll();
+	}
+
+	@Test
+	void getReviewsByProductId(){
+		int productId = 1;
+
+		ArrayList<ReviewEntity> entities = new ArrayList<>();
+		entities.add(new ReviewEntity(productId, 1, "a1", "s1", "c1"));
+		entities.add(new ReviewEntity(productId, 2, "a2", "s2", "c2"));
+		entities.add(new ReviewEntity(productId, 3, "a3", "s3", "c3"));
+
+		repository.saveAll(entities);
+
+		assertEquals(3, repository.findByProductId(productId).size());
+
+		getAndVerifyReviewsByProductId(productId, OK)
+				.jsonPath("$.length()").isEqualTo(3)
+				.jsonPath("$[2].productId").isEqualTo(productId)
+				.jsonPath("$[2].reviewId").isEqualTo(3);
+	}
+
 	//@Test
 	void getReviewsOk(){
 		int productId = 1;
 
-		webTestClient.get()
+		client.get()
 				.uri("/review?productId=" + productId)
 				.accept(APPLICATION_JSON)
 				.exchange()
@@ -43,7 +75,7 @@ class ReviewServiceApplicationTests extends MySqlTestBase {
 	void getReviewsNotFound() {
 		int productId = 213;
 
-		webTestClient.get()
+		client.get()
 				.uri("/review?productId=" + productId)
 				.accept(APPLICATION_JSON)
 				.exchange()
@@ -55,7 +87,7 @@ class ReviewServiceApplicationTests extends MySqlTestBase {
 
 	//@Test
 	void getReviewsMissingParameter(){
-		webTestClient.get()
+		client.get()
 				.uri("/review")
 				.accept(APPLICATION_JSON)
 				.exchange()
@@ -68,7 +100,7 @@ class ReviewServiceApplicationTests extends MySqlTestBase {
 
 	//@Test
 	void getReviewsInvalidParameter(){
-		webTestClient.get()
+		client.get()
 				.uri("/review?productId=no-integer")
 				.accept(APPLICATION_JSON)
 				.exchange()
@@ -83,7 +115,7 @@ class ReviewServiceApplicationTests extends MySqlTestBase {
 	void getReviewsInvalidParameterNegativeValue(){
 		int productId = -1;
 
-		webTestClient.get()
+		client.get()
 				.uri("/review?productId=" + productId)
 				.accept(APPLICATION_JSON)
 				.exchange()
@@ -94,4 +126,29 @@ class ReviewServiceApplicationTests extends MySqlTestBase {
 				.jsonPath("$.message").isEqualTo("Invalid productId: "  + productId);
 	}
 
+	private WebTestClient.BodyContentSpec getAndVerifyReviewsByProductId(int productId, HttpStatus expectedStatus) {
+		return getAndVerifyReviewsByProductId("?productId=" + productId, expectedStatus);
+	}
+
+	private WebTestClient.BodyContentSpec getAndVerifyReviewsByProductId(String productIdQuery, HttpStatus expectedStatus) {
+		return client.get()
+				.uri("/review" + productIdQuery)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(expectedStatus)
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody();
+	}
+
+	private WebTestClient.BodyContentSpec postAndVerifyReview(int productId, int reviewId, HttpStatus expectedStatus) {
+		Review review = new Review(productId, reviewId, "Author " + reviewId, "Subject " + reviewId, "Content " + reviewId, "SA");
+		return client.post()
+				.uri("/review")
+				.body(just(review), Review.class)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(expectedStatus)
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody();
+	}
 }
